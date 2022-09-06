01cda08cfc8cf8fe081f7a829bd2ebb81c083eec
ploy <plo_y@live.com>
2022-09-06 13:39:40 +0700

    add console log angle direction

--- pacman.js
+++ pacman.js
@@ -480,12 +480,16 @@ Pacman.User = function (game, map) {
    */
   function calcAngle(dir, pos) {
     if (dir == RIGHT && pos.x % 10 < 5) {
+      console.log("RIGHT");
       return { start: 0.25, end: 1.75, direction: false };
     } else if (dir === DOWN && pos.y % 10 < 5) {
+      console.log("DOWN");
       return { start: 0.75, end: 2.25, direction: false };
     } else if (dir === UP && pos.y % 10 < 5) {
+      console.log("UP");
       return { start: 1.25, end: 1.75, direction: true };
     } else if (dir === LEFT && pos.x % 10 < 5) {
+      console.log("LEFT");
       return { start: 0.75, end: 1.25, direction: true };
     }
     return { start: 0, end: 2, direction: false };
